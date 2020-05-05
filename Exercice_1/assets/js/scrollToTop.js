a0a63fb0bbd603f75f613baf95ffb64b87cbe226
Julien Gioux <jules.gioux@gmail.com>
2020-05-05 13:32:57 +0200

    turn & turn & turn

--- Exercice_1/assets/js/scrollToTop.js
+++ Exercice_1/assets/js/scrollToTop.js
@@ -1,13 +1,18 @@
-Btn_ScrollToTop.onclick = func_SrollToTop = () => {
+Btn_ScrollToTop.onclick = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 document.onscroll = () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        Btn_ScrollToTop.style.transform = `scale(100%) rotate(-90deg)`;
+        Btn_ScrollToTop.style.transform = `scale(100%) rotate(270deg)`;
+        Btn_ScrollToTop.style.opacity = `1`;
         Btn_ScrollToTop.style.transition = `0.5s`;
+        Btn_ScrollToTop.onmouseover = () => Btn_ScrollToTop.style.transform = `scale(150%) rotate(-450deg)`;
+        Btn_ScrollToTop.onmouseout = () => Btn_ScrollToTop.style.transform = `scale(100%) rotate(630deg)`;
+
       } else {
-        Btn_ScrollToTop.style.transform = `scale(0%) rotate(-90deg)`;        
+        Btn_ScrollToTop.style.transform = `scale(0%) rotate(-360deg)`;
+        Btn_ScrollToTop.style.opacity = `0`;        
         Btn_ScrollToTop.style.transition = `0.5s`;
       }
 }
